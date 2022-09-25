fec357623df8e618b8006036a5deef511ab156bf
sunil-ghimire <pox420@gmail.com>
2022-09-25 21:50:55 +0545

    Project setup done

---
+++ FactoryNepal/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FactoryNepal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FactoryNepal.settings')
+
+application = get_wsgi_application()
