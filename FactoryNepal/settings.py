c44c07166d86bf59331a1e08660e2dad7363ab8d
sunil-ghimire <pox420@gmail.com>
2022-12-15 16:43:29 +0545

    drf-yasg setup done

--- FactoryNepal/settings.py
+++ FactoryNepal/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'crispy_forms',
     'crispy_bootstrap5',
+    'drf-yasg',
 ]
 
 MIDDLEWARE = [
--- FactoryNepal/urls.py
+++ FactoryNepal/urls.py
@@ -19,7 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# drf_yasg code starts here
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Test API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="testing@api.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+# ends here
+
 urlpatterns = [
+    path('playground/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
 
