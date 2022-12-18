633cecdaa53ad21f7ef322a76d593b458cbde94f
sunil-ghimire <pox420@gmail.com>
2022-12-18 13:45:22 +0545

    api created for seller/user login

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,7 @@ router.register('products', ProductViewSet, basename='products')
 urlpatterns = [
     path('', include(router.urls)),
     path('create_user/', create_user, name='create_user'),
+    path('login_user/', login_user, name='login_user'),
+
     path('create_seller/', create_seller, name='create_seller')
 ]
--- api/views.py
+++ api/views.py
@@ -27,6 +27,21 @@ def create_user(request):
 
 
 @api_view(['POST'])
+def login_user(request):
+    email = request.data['email']
+    password = request.data['password']
+    user = User.objects.filter(email=email).first()
+    if user:
+        if user.check_password(password):
+            serializer = CreateUserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Wrong password'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
 def create_seller(request):
     serializer = CreateSellerSerializer(data=request.data)
     if serializer.is_valid():
